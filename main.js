3f69a06bdbbbb1179cb4d70b59c5a107a2cde09b
Jimmy-gmail <77905122+Jimmy-gmail@users.noreply.github.com>
2021-03-03 17:47:10 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var first_name= "ratan";
+console.log(first_name);
+
+var last_name= "Hatte";
+console.log(last_name);
+
+var full_name= first_name.concat(last_name);
+console.log(full_name);
+document.getElementById("full_name").innerHTML=full_name;
+
+var length_of_full_name= full_name.length;
+console.log(length_of_full_name);
+
+var name_one=" I Am John Carthwright";
+console.log(name_one);
+var replace_name= name_one.replace("John","Ratan" );
+console.log(replace_name);
+
+
